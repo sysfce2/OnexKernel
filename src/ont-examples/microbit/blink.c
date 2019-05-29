07f3c40c856e9ceb4975e01ca7d8e3b61e7ea04e
DuncanCragg <cyrus@cilux.org>
2019-05-29 20:39:35 +0100

    choose some LEDS for blinky

--- src/ont-examples/microbit/blink.c
+++ src/ont-examples/microbit/blink.c
@@ -28,7 +28,26 @@ int main()
   serial_printf("Type 'o' or 'i'\n");
   for(;;){
     serial_printf("%dms %d %d\n", time_ms(), speed, random_ish_byte());
+    gpio_set(leds_list[3], 0);
+    gpio_set(leds_list[10], 0);
+    time_delay_ms(speed);
+    gpio_set(leds_list[3], 1);
+    gpio_set(leds_list[10], 1);
+    time_delay_ms(speed);
+/*
     for(uint8_t l=0; l<LEDS_NUMBER; l++){ gpio_toggle(leds_list[l]); time_delay_ms(speed); }
+
+    for(uint8_t r=0; r<LEDS_NUMBER; r++){
+      gpio_set(leds_list[r], 0);
+      for(uint8_t c=0; c<LEDS_NUMBER; c++){
+        serial_printf("row %d col %d\n", r, c);
+        gpio_set(leds_list[c], 0);
+        time_delay_ms(speed);
+        gpio_set(leds_list[c], 1);
+      }
+      gpio_set(leds_list[r], 1);
+    }
+*/
   }
 }
 
