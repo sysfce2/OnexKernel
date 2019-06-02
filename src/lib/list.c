e7d9f37c485d020c156d00e9ae4ef778d5525bf6
DuncanCragg <cyrus@cilux.org>
2019-06-02 18:40:59 +0100

    i->size

--- src/lib/list.c
+++ src/lib/list.c
@@ -19,7 +19,7 @@ typedef struct list {
   item_type type;
   uint16_t max_size;
   void**  vals;
-  uint16_t i;
+  uint16_t size;
 } list;
 
 list* list_new(uint16_t max_size)
@@ -30,7 +30,7 @@ list* list_new(uint16_t max_size)
   li->max_size=max_size;
   li->vals=(void**)calloc(max_size,sizeof(void*));
   if(!li->vals) return 0;
-  li->i=0;
+  li->size=0;
   return li;
 }
 
@@ -51,16 +51,16 @@ list* list_new_from(char* text, uint16_t max_size)
 bool list_add(list* li, void* val)
 {
   if(!li) return false;
-  if(li->i==li->max_size) return false;
-  li->vals[li->i]=val;
-  li->i++;
+  if(li->size==li->max_size) return false;
+  li->vals[li->size]=val;
+  li->size++;
   return true;
 }
 
 bool list_set_n(list* li, uint16_t index, void* val)
 {
   if(!li) return false;
-  if(index<=0 || index>li->i) return false;
+  if(index<=0 || index>li->size) return false;
   li->vals[index-1]=val;
   return true;
 }
@@ -68,27 +68,27 @@ bool list_set_n(list* li, uint16_t index, void* val)
 void* list_get_n(list* li, uint16_t index)
 {
   if(!li) return 0;
-  if(index<=0 || index>li->i) return 0;
+  if(index<=0 || index>li->size) return 0;
   return li->vals[index-1];
 }
 
 void* list_del_n(list* li, uint16_t index)
 {
   if(!li) return 0;
-  if(index<=0 || index>li->i) return 0;
+  if(index<=0 || index>li->size) return 0;
   int j=index-1;
   void* v=li->vals[j];
-  for(; j < li->i-1; j++){
+  for(; j < li->size-1; j++){
     li->vals[j] = li->vals[j+1];
   }
-  li->i--;
+  li->size--;
   return v;
 }
 
 uint16_t list_size(list* li)
 {
   if(!li) return 0;
-  return li->i;
+  return li->size;
 }
 
 void list_free(list* li)
@@ -100,13 +100,13 @@ void list_free(list* li)
 char* list_to_text(list* li, char* b, uint16_t s)
 {
   *b=0;
-  if(!li || !li->i) return b;
+  if(!li || !li->size) return b;
   int ln=0;
   int j;
-  for(j=0; j<li->i; j++){
+  for(j=0; j<li->size; j++){
     ln+=strlen(item_to_text(li->vals[j], b+ln, s-ln));
     if(ln>=s){ *b = 0; return b; }
-    if(j!=li->i-1) ln+=snprintf(b+ln, s-ln, " ");
+    if(j!=li->size-1) ln+=snprintf(b+ln, s-ln, " ");
     if(ln>=s){ *b = 0; return b; }
   }
   return b;
