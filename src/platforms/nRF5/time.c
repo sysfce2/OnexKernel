de383c64833d5855a96a6ca7357e1fc7f0a323b1
DuncanCragg <duncan@cilux.org>
2020-02-27 13:10:48 +0000

    use app_timer for time_ms()

--- src/platforms/nRF5/time.c
+++ src/platforms/nRF5/time.c
@@ -7,6 +7,15 @@
 
 static bool initialised=false;
 
+APP_TIMER_DEF(m_timer_0);
+
+uint32_t counter;
+
+static void timer_handle(void* p)
+{
+  counter++;
+}
+
 void time_init()
 {
   if(initialised) return;
@@ -14,20 +23,26 @@ void time_init()
   nrf_drv_clock_lfclk_request(NULL);
   while(!nrf_drv_clock_lfclk_is_running());
   e = app_timer_init(); APP_ERROR_CHECK(e);
+  e = app_timer_create(&m_timer_0, APP_TIMER_MODE_REPEATED, timer_handle); APP_ERROR_CHECK(e);
+  e = app_timer_start(m_timer_0, APP_TIMER_TICKS(1000), NULL); APP_ERROR_CHECK(e);
   initialised=true;
 }
 
+#define APP_TIMER_PRESCALER 0 // not sure what this is
+
 uint32_t time_ms(){
-  return 0;
+  uint32_t ticks=app_timer_cnt_get();
+  return ticks*((APP_TIMER_PRESCALER+1)*1000)/APP_TIMER_CLOCK_FREQ;
 }
 
 uint32_t time_us(){
-  return 0;
+  uint32_t ticks=app_timer_cnt_get();
+  return ticks*((APP_TIMER_PRESCALER+1)*1000000L)/APP_TIMER_CLOCK_FREQ;
 }
 
 void time_delay_us(uint32_t us)
 {
-  nrf_delay_us(us);
+  nrf_delay_us(us*2); // *2??
 }
 
 void time_delay_ms(uint32_t ms)
