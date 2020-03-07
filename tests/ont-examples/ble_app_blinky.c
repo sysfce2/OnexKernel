302e1068c8cbc4f7640edc1bd60f222197cdaeda
DuncanCragg <duncan@cilux.org>
2020-03-07 10:00:30 +0000

    remove power management from blinky

--- tests/ont-examples/ble_app_blinky.c
+++ tests/ont-examples/ble_app_blinky.c
@@ -4,7 +4,6 @@
 #include "nordic_common.h"
 #include "nrf.h"
 #include "boards.h"
-#include "nrf_pwr_mgmt.h"
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
@@ -24,25 +23,8 @@ void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
     app_error_handler(DEAD_BEEF, line_num, p_file_name);
 }
 
-static void power_management_init(void)
-{
-    ret_code_t err_code;
-    err_code = nrf_pwr_mgmt_init();
-    APP_ERROR_CHECK(err_code);
-}
-
-static void idle_state_handle(void)
-{
-    if (NRF_LOG_PROCESS() == false)
-    {
-        nrf_pwr_mgmt_run();
-    }
-}
-
 int main()
 {
-    power_management_init();
-
     log_init();
     time_init();
 #ifdef HAS_SERIAL
@@ -57,7 +39,6 @@ int main()
 #ifdef HAS_SERIAL
       serial_loop();
 #endif
-      idle_state_handle();
     }
 }
 
