61b2edde6a928ccda465796e48bff404f40fc70a
DuncanCragg <duncan@cilux.org>
2020-03-15 13:01:18 +0000

    non-overflowing time :-)

--- src/platforms/nRF5/time.c
+++ src/platforms/nRF5/time.c
@@ -9,11 +9,18 @@ static bool initialised=false;
 
 APP_TIMER_DEF(m_timer_0);
 
-uint32_t counter;
 
-static void timer_handle(void* p)
+#define APP_TIMER_PRESCALER 1 // where is this defined?
+#define EFFECTIVE_TIMER_CLOCK_FREQ (APP_TIMER_CLOCK_FREQ/(APP_TIMER_PRESCALER+1))
+#define OVERFLOW_TICKS 16777216
+
+static uint64_t seconds=0;
+static uint32_t ticks_at_second=0;
+
+static void every_second(void* p)
 {
-  counter++;
+  seconds++;
+  ticks_at_second = app_timer_cnt_get();
 }
 
 void time_init()
@@ -23,21 +30,23 @@ void time_init()
   nrf_drv_clock_lfclk_request(NULL);
   while(!nrf_drv_clock_lfclk_is_running());
   e = app_timer_init(); APP_ERROR_CHECK(e);
-  e = app_timer_create(&m_timer_0, APP_TIMER_MODE_REPEATED, timer_handle); APP_ERROR_CHECK(e);
+  e = app_timer_create(&m_timer_0, APP_TIMER_MODE_REPEATED, every_second); APP_ERROR_CHECK(e);
   e = app_timer_start(m_timer_0, APP_TIMER_TICKS(1000), NULL); APP_ERROR_CHECK(e);
   initialised=true;
 }
 
-#define APP_TIMER_PRESCALER 1 // not sure what this is but it has to be one
+uint64_t time_s(){
+  return seconds;
+}
 
 uint64_t time_ms(){
-  uint64_t ticks=app_timer_cnt_get();
-  return ticks*((APP_TIMER_PRESCALER+1)*1000)/APP_TIMER_CLOCK_FREQ;
+  int32_t ticks = app_timer_cnt_get()-ticks_at_second;
+  if(ticks<0) ticks+=OVERFLOW_TICKS;
+  return (seconds*1000)+(ticks*1000)/EFFECTIVE_TIMER_CLOCK_FREQ;
 }
 
 uint64_t time_us(){
-  uint64_t ticks=app_timer_cnt_get();
-  return ticks*((APP_TIMER_PRESCALER+1)*1000000L)/APP_TIMER_CLOCK_FREQ;
+  return time_ms()*1000;
 }
 
 void time_delay_us(uint32_t us)
