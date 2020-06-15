4fe8e2edb9d315b933d626b47dd348fe08e82629
DuncanCragg <duncan@cilux.org>
2020-06-15 15:18:31 +0100

    free for [mc]alloc

--- src/platforms/nRF5/mem.c
+++ src/platforms/nRF5/mem.c
@@ -13,8 +13,8 @@ void* Mem_alloc(char* func, int line, size_t n)
   if(!initialized){ initialized=true; nrf_mem_init(); }
   void* p=nrf_calloc(1,n);
   if(!p){
-    if(LOG_MEM) log_write("****** mem_alloc using calloc");
     p=calloc(1,n);
+    if(LOG_MEM) log_write("****** mem_alloc using calloc %p", p);
   }
   if(LOG_MEM) log_write("mem_alloc   %p %lu %s:%d\n", p, n, func, line);
   return p;
@@ -24,7 +24,10 @@ void  Mem_free(char* func, int line, void* p)
 {
   if(!initialized){ initialized=true; nrf_mem_init(); }
   if(LOG_MEM) log_write("mem_free    %p %lu %s:%d\n", p, (size_t)0, func, line);
-  nrf_free(p);
+  if(nrf_free(p)){
+    if(LOG_MEM) log_write("****** mem_free using free %p", p);
+    free(p);
+  }
 }
 
 char* Mem_strdup(char* func, int line, const char* s)
@@ -33,8 +36,8 @@ char* Mem_strdup(char* func, int line, const char* s)
   size_t n=strlen(s)+1;
   char* p=nrf_malloc(n);
   if(!p){
-    if(LOG_MEM) log_write("****** mem_strdup using malloc");
     p=malloc(n);
+    if(LOG_MEM) log_write("****** mem_strdup using malloc %p", p);
   }
   if(p) memcpy(p,s,n);
   char b[20]; mem_strncpy(b, s, 18);
@@ -49,7 +52,10 @@ void Mem_freestr(char* func, int line, char* p)
   size_t n=strlen(p)+1;
   char b[20]; mem_strncpy(b, p, 18);
   if(LOG_MEM) log_write("mem_freestr %p %lu [%s] %s:%d\n", p, n, b, func, line);
-  nrf_free(p);
+  if(nrf_free(p)){
+    if(LOG_MEM) log_write("****** mem_freestr using free %p", p);
+    free(p);
+  }
 }
 
 void mem_strncpy(char* dst, const char* src, size_t count)
