a1a16689afa75e54fb4a799bb3f031defe9c5458
DuncanCragg <duncan@cilux.org>
2023-01-30 22:06:53 +0000

    drawing into a buffer, again, ATC's code :-)

--- include/onex-kernel/gfx.h
+++ include/onex-kernel/gfx.h
@@ -35,5 +35,10 @@ void gfx_pop();
 
 void gfx_text(char* text);
 
+void gfx_fast_init();
+void gfx_fast_clear_screen(uint8_t colour); // 8 bits * 2, so 0x00 => 0x0000, 0x77 => 0x7777, etc
+void gfx_fast_text(int32_t x, int32_t y, char* text, uint16_t colour, uint16_t bg, uint32_t size);
+void gfx_fast_write_out_buffer();
+
 #endif
 
