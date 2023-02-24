eea07509b1fcf2dd6b4667da5bd9bb93e3c82bc2
DuncanCragg <duncan@cilux.org>
2023-02-24 20:42:15 +0000

    unix logging file/line

--- src/onl/unix/log.c
+++ src/onl/unix/log.c
@@ -13,8 +13,8 @@ bool log_loop()
   return false;
 }
 
-int log_write(const char* fmt, ...)
-{
+int log_write_current_file_line(char* file, uint32_t line, const char* fmt, ...) {
+  if(file) printf("%s:%d:", file, line);
   va_list args;
   va_start(args, fmt);
   int r=vfprintf(stdout, fmt, args);
