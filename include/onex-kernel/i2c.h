9c32a4955200cc9b19109e312e19bdcffb25c425
DuncanCragg <duncan@cilux.org>
2025-05-15 21:39:04 +0100

    i2c_write_register_hi_lo

--- include/onex-kernel/i2c.h
+++ include/onex-kernel/i2c.h
@@ -4,13 +4,15 @@
 #include <stdint.h>
 
 void*   i2c_init(uint16_t speedk);
-uint8_t i2c_read(               void* twip, uint8_t address,              uint8_t* buf, uint16_t len);
-uint8_t i2c_write(              void* twip, uint8_t address,              uint8_t* buf, uint16_t len);
-uint8_t i2c_read_register(      void* twip, uint8_t address, uint8_t reg, uint8_t* buf, uint16_t len);
-uint8_t i2c_write_register(     void* twip, uint8_t address, uint8_t reg, uint8_t* buf, uint16_t len);
-uint8_t i2c_write_register_byte(void* twip, uint8_t address, uint8_t reg, uint8_t val);
-uint8_t i2c_read_register_hi_lo(void* twip, uint8_t address, uint8_t reg_hi,
-                                                             uint8_t reg_lo, uint8_t* buf, uint16_t len);
+uint8_t i2c_read(                void* twip, uint8_t address,              uint8_t* buf, uint16_t len);
+uint8_t i2c_write(               void* twip, uint8_t address,              uint8_t* buf, uint16_t len);
+uint8_t i2c_read_register(       void* twip, uint8_t address, uint8_t reg, uint8_t* buf, uint16_t len);
+uint8_t i2c_write_register(      void* twip, uint8_t address, uint8_t reg, uint8_t* buf, uint16_t len);
+uint8_t i2c_write_register_byte( void* twip, uint8_t address, uint8_t reg, uint8_t val);
+uint8_t i2c_read_register_hi_lo( void* twip, uint8_t address, uint8_t reg_hi,
+                                                              uint8_t reg_lo, uint8_t* buf, uint16_t len);
+uint8_t i2c_write_register_hi_lo(void* twip, uint8_t address, uint8_t reg_hi,
+                                                              uint8_t reg_lo, uint8_t* buf, uint16_t len);
 void    i2c_sleep();
 void    i2c_wake();
 
